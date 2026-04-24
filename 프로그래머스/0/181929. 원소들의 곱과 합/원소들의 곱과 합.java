32531497fceff82384b41a1e6d774be53be07898
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-24 13:47:06 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.10 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(int[] num_list) {
-        int sum1 = 1;
-        int sum2 = 0;
+        int sum = 1;
+        int mult = 0;
         for(int i = 0; i < num_list.length; i++) {
-            sum1 *= num_list[i];
-            sum2 += num_list[i];
+            sum *= num_list[i];
+            mult += num_list[i];
         }
-        int answer = sum1 < Math.pow(sum2, 2) ? 1 : 0;
+        int answer = sum < Math.pow(mult, 2) ? 1 : 0;
         return answer;
     }
 }
