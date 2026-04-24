0aecd2ba99e6a17cb300ed1149b13c83c0f57e4c
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-24 17:47:41 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
-        int cnt = 0;
         for(int i = 0; i < num_list.length; i++) {
-            cnt += num_list[i] == n ? 1 : 0;
-            if(cnt >= 1) {
-                answer = 1;
-            } else {
-                answer = 0;
+            if(num_list[i] == n) {
+                return 1;
             }
         }
-        return answer;
+        return 0;
     }
 }
