ec381baa6028da17cffafdc78a400182f503fbcc
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-24 10:30:34 +0900

    [level 0] Title: 코드 처리하기, Time: 577.24 ms, Memory: 379 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String code) {
+        int mode = 0;
+        String ret = "";
+        for(int idx = 0; idx < code.length(); idx++) {
+            char c = code.charAt(idx);
+            if(mode == 0) {
+                ret += c != '1' && idx % 2 == 0 ? c : "";
+                mode = c == '1' ? 1 : 0;
+            } else if (mode == 1) {
+                ret += c != '1' && idx % 2 == 1 ? c : "";
+                mode = c == '1' ? 0 : 1;
+            }
+        }
+        
+        return ret = ret.equals("") ? "EMPTY" : ret;
+    }
+}
