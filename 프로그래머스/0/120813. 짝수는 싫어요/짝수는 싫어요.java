43d19f6ceccc5541ab042212c94197780dbf3acc
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-26 21:51:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1) / 2];
+        int j = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 1) {
+                answer[j] = i;
+                j += 1;
+            }
+        }
+        return answer;
+    }
+}
