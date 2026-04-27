b586114f45b5db9207f5d0e0d502d84b4492b709
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-27 17:25:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 1150.04 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -4,13 +4,14 @@ class Solution {
         for(int i = 0; i < numLog.length - 1; i++) {
             int num = numLog[i+1] - numLog[i];
             switch(num) {
-                case -10: answer += "a";
+                case 1: answer += "w";
                     break;
                 case -1: answer += "s";
                     break;
-                case 1: answer += "w";
+                case -10: answer += "a";
                     break;
                 case 10: answer += "d";
+                    break;
             }
         }
         return answer;
