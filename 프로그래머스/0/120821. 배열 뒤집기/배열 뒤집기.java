f6035eaca70367ca6bf30239722f3e53333af787
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-04-30 09:05:56 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int temp = 0;
+        for(int i = 0; i <= num_list.length / 2; i++) {
+            temp = num_list[i];
+            num_list[i] = num_list[num_list.length - i - 1];
+            num_list[num_list.length - i - 1] = temp;
+        }
+        return num_list;
+    }
+}
