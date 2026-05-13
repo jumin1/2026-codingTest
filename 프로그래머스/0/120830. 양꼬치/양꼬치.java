72c1a9439674d656d7778dc4b86f09d1b92bc339
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-05-13 17:41:49 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int n, int k) {
-        int cnt = n / 10;
-        return n * 12000 + (k - cnt) * 2000;
+        int service = n / 10;
+        n *= 12000;
+        k = (k - service) * 2000;
+        return n + k;
     }
 }
