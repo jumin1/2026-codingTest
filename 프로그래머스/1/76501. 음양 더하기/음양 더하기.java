b80816bb97d06e95cad15e88688552e6a7327bec
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-05-28 15:29:42 +0900

    [level 1] Title: 음양 더하기, Time: 0.03 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,8 +2,7 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int total = 0;
         for(int i = 0; i < absolutes.length; i++) {
-            if(signs[i] == false) absolutes[i] *= -1;
-            total += absolutes[i];
+            total += absolutes[i] * (signs[i] ? 1 : -1);
         }
         return total;
     }
