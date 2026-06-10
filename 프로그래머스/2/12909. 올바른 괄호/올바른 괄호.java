59e2a8cd7a598523818b49219a44545dca9fda9d
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-06-10 16:56:17 +0900

    [level 2] Title: 올바른 괄호, Time: 2.94 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,7 +1,7 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
         int count = 0;
+        
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '(') {
                 count++;
@@ -12,11 +12,7 @@ class Solution {
                 return false;
             }
         }
-        boolean check1 = count == 0;
-        boolean check2 = s.charAt(0) == '(';
-        if(check1 && check2) {
-            answer = true;
-        }
-        return answer;
+        
+        return count == 0;
     }
 }
