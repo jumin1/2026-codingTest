dcdca47ed36107a9cb89d3d34e60695d06af2abe
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-06-15 16:35:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == ' ') {
+                index = 0;
+                sb.append(c);
+            } else {
+                sb.append(index % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                index++;
+            }
+        }
+        return sb.toString();
+    }
+}
