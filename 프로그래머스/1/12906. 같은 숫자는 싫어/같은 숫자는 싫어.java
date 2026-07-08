423cc19bcc1b5153abc0012b967514ef70486244
jumin1 <144890643+jumin1@users.noreply.github.com>
2026-07-08 14:22:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.92 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,20 +3,20 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         int count = 0;
-        int d1 = arr[0];
+        int d = arr[0];
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] != d1) {
-                d1 = arr[i];
+            if(arr[i] != d) {
+                d = arr[i];
                 count++;
             }
         }
         int[] answer = new int[count + 1];
-        int d2 = arr[0];
+        d = arr[0];
         answer[0] = arr[0];
         int idx = 1;
-        for(int j = 0; j < arr.length; j++) {
-            if(arr[j] != d2) {
-                d2 = arr[j];
+        for(int j = 1; j < arr.length; j++) {
+            if(arr[j] != d) {
+                d = arr[j];
                 answer[idx++] = arr[j];
             }
         }
